bb03579417f8ecaee78773e91cbe35c9cafe444c
heinrich-marx <weriast98@gmail.com>
2021-07-27 12:27:02 +0300

    feat:complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.average = Math.ceil((this.min + this.max)/2)
+        return this.average
     }
 
     lower() {
-
+        this.max = this.average
     }
 
     greater() {
-
+        this.min = this.average 
     }
 }
 
